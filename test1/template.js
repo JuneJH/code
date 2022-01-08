379c780bc37eda100cef33e7e173a67258c8d9c3
JuneJH <15310487388@163.com>
2022-01-08 15:30:41 +0800

    fix(*): 移除模版

--- test1/template.js
+++ test1/template.js
@@ -530,3 +530,32 @@ proxyImage.setSrc('https://inews.gtimg.com/newsapp_bt/0/11205804124/1000')
 // 19. 装饰者模式
 
 // 20. 
+
+
+// 1.new  objectFactory
+
+// 2.大数相加 add
+
+// 3. 树深度优先 非递归deepTravel  递归dfs
+
+// 4. 树广度优先  非递归reBfs  递归bfs
+
+// 5. 单旋singleRotate
+// 无法解决变化分支不可以是唯一最深支
+// 左浅右深 左单旋
+// 做单旋步骤
+// 1. 找到新根   root.right
+// 2. 找到变化分支  新根.left
+// 3. 当前旋转节点的右孩子是变化分支 
+// 4. 新根的左孩子是旋转节点
+// 5. 返回新的根节点
+
+// 6，图的广度bfsGraph
+
+// 7. 图的深度deepSearchGraph
+
+// 8. kruskal
+
+// 9. Events
+
+// 10. reduce实现map
