17b9555f8a6cb5ccf3a8a4b38afb64c715bc3050
JuneJH <15310487388@163.com>
2022-01-18 23:18:45 +0800

    feat(*): 增加二叉树遍历

--- algorithm/binaryTree/__test__/utils.test.js
+++ algorithm/binaryTree/__test__/utils.test.js
@@ -1,12 +1,49 @@
-const {Node,getTreeDeep} = require("../src/utils")
+const {Node,
+       getTreeDeep,
+       preTraverseTree,
+       preTraverseTreeRecursion,
+       midTraverseTreeRecursion,
+       midTraverseTree,
+       afterTraverseTreeRecursion,
+
+} = require("../src/utils")
 
 describe("测试二叉树工具方法",()=>{
-    const a = new Node('a')
-    const b = new Node('b')
-    const c = new Node('c');
+    const a = new Node(1); //           1
+    const b = new Node(2); //      2             3
+    const c = new Node(3); //  4       5     6       7
+    const d = new Node(4);
+    const e = new Node(5);
+    const f = new Node(6);
+    const g = new Node(7);
     a.left = b;
     a.right = c;
-  it("测试链表长度",()=>{
-    expect(getTreeDeep(a)).toBe(2);
-  })
+
+    b.left = d;
+    b.right = e;
+
+    c.left = f;
+    c.right = g;
+
+  it("测试二叉树长度",()=>{
+    expect(getTreeDeep(a)).toBe(3);
+  });
+  it("测试二叉树先序迭代遍历",()=>{
+      expect(preTraverseTree(a)).toMatchObject([1,2,4,5,3,6,7])
+  });
+  it("测试二叉树先序递归遍历",()=>{
+      expect(preTraverseTreeRecursion(a)).toMatchObject([1,2,4,5,3,6,7])
+  });
+  it("测试二叉树中序递归遍历",()=>{
+      expect(midTraverseTreeRecursion(a)).toMatchObject([4,2,5,1,6,3,7])
+  });
+  it("测试二叉树中序迭代遍历",()=>{
+      expect(midTraverseTree(a)).toMatchObject([4,2,5,1,6,3,7])
+  });
+  it("测试二叉树后序递归遍历",()=>{
+      expect(afterTraverseTreeRecursion(a)).toMatchObject([4,5,2,6,7,3,1])
+  });
+  it("测试二叉树后序迭代遍历",()=>{
+      expect(midTraverseTree(a)).toMatchObject([4,2,5,1,6,3,7])
+  });
 })
--- algorithm/binaryTree/src/utils.js
+++ algorithm/binaryTree/src/utils.js
@@ -1,9 +1,9 @@
 /**
  * 构造一个二叉树
- * @param {*} value 
+ * @param {*} val
  */
-function Node(value) {
-    this.value = value;
+function Node(val) {
+    this.val = val;
     this.left = null;
     this.right = null;
 }
@@ -17,7 +17,105 @@ function getTreeDeep(root) {
     return Math.max(getTreeDeep(root.left), getTreeDeep(root.right)) + 1;
 }
 
+/**
+ * 先序遍历 【递归】
+ * @param root
+ * @returns {*[]}
+ */
+function  preTraverseTreeRecursion(root){
+    const result = []
+    function  _fn(root){
+        if(root === null){
+            return;
+        }
+        result.push(root.val);
+        _fn(root.left);
+        _fn(root.right)
+    }
+    _fn(root)
+    return  result;
+}
+function  preTraverseTree(root){
+   const stack = [],res = [];
+   while (root || stack.length > 0){
+       while (root){
+           res.push(root.val)
+           stack.push(root.right);
+           root = root.left;
+       }
+       root = stack.pop();
+   }
+   return res;
+}
+
+/**
+ * 中序遍历
+ * @param root
+ */
+function midTraverseTreeRecursion(root){
+    const res = [];
+    function  _fn(root){
+        if(root === null){
+            return;
+        }
+        _fn(root.left);
+        res.push(root.val);
+        _fn(root.right);
+    }
+    _fn(root);
+    return res;
+}
+function midTraverseTree(root){
+    const res = [],stack = [];
+    while (root || stack.length > 0){
+        while (root){
+            stack.push(root);
+            root = root.left;
+        }
+        root = stack.pop();
+        res.push(root.val);
+        root = root.right;
+    }
+    return res;
+}
+
+/**
+ * 后序遍历
+ * @param root
+ * @returns {*[]}
+ */
+function afterTraverseTreeRecursion(root){
+    const res = [];
+    function  _fn(root){
+        if(root === null){
+            return;
+        }
+        _fn(root.left);
+        _fn(root.right);
+        res.push(root.val);
+    }
+    _fn(root);
+    return res;
+}
+function afterTraverseTree(root){
+    const res = [],stack = [];
+    while (root || stack.length > 0){
+        while (root){
+            stack.push(root);
+            root = root.left;
+        }
+        root = stack.pop();
+        res.push(root.val);
+
+    }
+}
+
 module.exports = {
     Node,
-    getTreeDeep
+    getTreeDeep,
+    preTraverseTree,
+    preTraverseTreeRecursion,
+    midTraverseTreeRecursion,
+    midTraverseTree,
+    afterTraverseTreeRecursion
 }
