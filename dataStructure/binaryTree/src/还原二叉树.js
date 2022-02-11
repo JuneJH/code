f43ad1c231eb208a7563a6ff51985409176c9757
JuneJH <15310487388@163.com>
2022-02-11 22:57:49 +0800

    feat(*): 新增还原二叉树签名

---
+++ dataStructure/binaryTree/src/还原二叉树.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+
+/**
+ * 根据前序中序还原二叉树并写出后序
+ * @param {*} pre 前序遍历
+ * @param {*} mid 中序遍历
+ */
+function reductionTree4preAndMid(pre,mid){
+
+}
+
+/**
+ * 根据中序后序还原二叉树并写出前序
+ * @param {*} mid 
+ * @param {*} after 
+ */
+function reductionTree4MidAndAfter(mid,after){
+
+}
+
+
+
+module.exports = {
+    reductionTree4preAndMid,
+    reductionTree4MidAndAfter
+}
+
