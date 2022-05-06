4d59b998c6396a2f21f232a2d8ce1d73545f14f8
JuneJH <15310487388@163.com>
2022-05-06 23:05:38 +0800

    docs(*): 整理数据结构

--- algorithm/位运算/src/sum.js
+++ algorithm/位运算/src/sum.js
@@ -1,11 +1,19 @@
-// 1.不适用四则运算求和
+/**
+ * 通过位运算请和
+ * @param {*} a 
+ * @param {*} b 
+ * @returns a,b的和
+ */
 function sum(a, b) {
-    if (a == 0) return b
-    if (b == 0) return a
-    let newA = a ^ b
-    let newB = (a & b) << 1
-    return sum(newA, newB)
+    if (a == 0) return b;
+    if (b == 0) return a;
+    let newA = a ^ b; // 未进位的加法
+    let newB = (a & b) << 1; // 进位后的结果
+    return sum(newA, newB);// 迭代
 }
+
+
+
 module.exports = {
     sum,
 }
