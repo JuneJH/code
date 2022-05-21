9f8ba62f36e3c97c16ad4fbca78f80946016b9ce
JuneJH <15310487388@163.com>
2022-05-22 00:14:07 +0800

    feat(*): 分治算法

--- sort/src/utils.js
+++ sort/src/utils.js
@@ -1,4 +1,16 @@
+/**
+ * 比较两个数值
+ * @param {*} a number
+ * @param {*} b 
+ * @returns 
+ */
 const compare = (a, b) => a > b ? true : false;
+/**
+ * 根据数组下标交换数组位置
+ * @param {*} arr 
+ * @param {*} a 
+ * @param {*} b 
+ */
 const change = (arr, a, b) => { [arr[a], arr[b]] = [arr[b], arr[a]] }
 
 
