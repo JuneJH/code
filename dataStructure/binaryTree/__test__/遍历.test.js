f393925f0e33a723b574a688acc0ce89ff7b88c2
JuneJH <15310487388@163.com>
2022-05-25 22:29:23 +0800

    feat(*): 中序遍历

--- dataStructure/binaryTree/__test__/遍历.test.js
+++ dataStructure/binaryTree/__test__/遍历.test.js
@@ -1,4 +1,5 @@
 const {preOrder,preOrder4Stack} = require("../src/先序遍历");
+const {ldr,ldr4stack} = require("../src/中序遍历");
 const {Node} = require("../src/utils")
 
 
@@ -21,4 +22,25 @@ test("先序遍历",()=>{
     const _a = a.left;
     expect(preOrder(a)).toMatchObject([1,2,4,5,3,6,7]);
     expect(preOrder4Stack(a)).toMatchObject([1,2,4,5,3,6,7]);
+})
+
+test("中序遍历",()=>{
+    const a = new Node(1); //           1
+    const b = new Node(2); //      2             3
+    const c = new Node(3); //  4       5     6       7
+    const d = new Node(4);
+    const e = new Node(5);
+    const f = new Node(6);
+    const g = new Node(7);
+    a.left = b;
+    a.right = c;
+
+    b.left = d;
+    b.right = e;
+
+    c.left = f;
+    c.right = g;
+    const _a = a.left;
+    expect(ldr(a)).toMatchObject([4,2,5,1,6,3,7]);
+    expect(ldr4stack(a)).toMatchObject([4,2,5,1,6,3,7]);
 })
--- dataStructure/binaryTree/src/中序遍历.js
+++ dataStructure/binaryTree/src/中序遍历.js
@@ -1,2 +1,34 @@
 
 
+function ldr(root,result = []){
+    if(root == null){
+        return result;
+    }
+    ldr(root.left,result);
+    result.push(root.val);
+    ldr(root.right,result);
+    return result;
+}
+
+function ldr4stack(root){
+    const result = [];
+    if(root == null){
+        return result;
+    }
+    const stack = new Array();
+    while(root || stack.length > 0){
+        while(root){
+            stack.push(root);
+            root = root.left;
+        }
+        root = stack.pop();
+        result.push(root.val);
+        root = root.right;
+    }
+    return result;
+}
+
+module.exports = {
+    ldr,
+    ldr4stack
+}
