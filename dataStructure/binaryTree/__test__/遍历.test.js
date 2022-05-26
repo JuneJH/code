bb165471c726ced7c9204c8c6892dbbc0e9c90a5
JuneJH <15310487388@163.com>
2022-05-26 23:33:31 +0800

    feat(*): 后序遍历

--- dataStructure/binaryTree/__test__/遍历.test.js
+++ dataStructure/binaryTree/__test__/遍历.test.js
@@ -1,5 +1,6 @@
 const {preOrder,preOrder4Stack} = require("../src/先序遍历");
 const {ldr,ldr4stack} = require("../src/中序遍历");
+const {lrd} = require("../src/后序遍历");
 const {Node} = require("../src/utils")
 
 
@@ -43,4 +44,27 @@ test("中序遍历",()=>{
     const _a = a.left;
     expect(ldr(a)).toMatchObject([4,2,5,1,6,3,7]);
     expect(ldr4stack(a)).toMatchObject([4,2,5,1,6,3,7]);
+
+   
 })
+
+test("后序遍历",()=>{
+    const a = new Node(1); //           1
+    const b = new Node(2); //      2             3
+    const c = new Node(3); //  4       5     6       7
+    const d = new Node(4);
+    const e = new Node(5);
+    const f = new Node(6);
+    const g = new Node(7);
+    a.left = b;
+    a.right = c;
+
+    b.left = d;
+    b.right = e;
+
+    c.left = f;
+    c.right = g;
+    const _a = a.left;
+    expect(lrd(a)).toMatchObject([4,5,2,6,7,3,1]);
+    expect(lrd(a)).toMatchObject([4,5,2,6,7,3,1]);
+});
--- dataStructure/binaryTree/src/后序遍历.js
+++ dataStructure/binaryTree/src/后序遍历.js
@@ -1,2 +1,13 @@
+function lrd(root,result = []){
+    if(root === null){
+        return ;
+    }
 
-
+    lrd(root.left,result);
+    lrd(root.right,result);
+    result.push(root.val);
+    return result;
+}
+module.exports = {
+    lrd
+}
