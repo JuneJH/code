42faf15caa245767e0647a0eb3800ceccc3aafde
JuneJH <15310487388@163.com>
2023-07-12 22:46:53 +0800

    feat(*): 整理

--- coding/redux/middleware/thunk.js
+++
@@ -1,9 +0,0 @@
-export function thunk({getState,dispatch}){
-    return next=>action=>{
-        if(typeof action === "function"){
-            return action(dispatch,getState);
-        }
-        return next(action);
-    }
-
-}
