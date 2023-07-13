0b97aeaf1c62157df6c255d76ceadc895c3b5e13
JuneJH <15310487388@163.com>
2023-07-13 23:31:03 +0800

    feat(*):优化遍历

--- dataStructure/binaryTree/src/广度优先搜索.js
+++ dataStructure/binaryTree/src/广度优先搜索.js
@@ -14,12 +14,44 @@ function  breadthSearch(root,target){
         if(node.val === target){
             return  true;
         }else{
-            node.left && children.push(node.left);
+            node.left && children.push(node.left); 
             node.right && children.push(node.right);
         }
     }
     return  breadthSearch(children,target);
 }
+
+function breadthSearch2(root){
+    const res = [];
+    if(root === null){
+        return res;
+    }
+
+    const queue = new Array(root);
+
+    while(queue.length > 0){
+        const node = queue.pop();
+        res.push(node.val);
+        node.left && queue.push(node.left);
+        node.right && queue.push(node.right);
+    }
+
+    return res;
+    
+}
+const {Node} = require("./utils")
+const a = new Node("1");
+const b = new Node("2");
+const c = new Node("3");
+const d = new Node("4");
+const e = new Node("5");
+
+a.left = b;
+a.right = c;
+b.left = d;
+d.right =e;
+
+console.log("runn",breadthSearch2(a))
 
 module.exports = {
     breadthSearch
