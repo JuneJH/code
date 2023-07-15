874ef0a3b0d50b6b02f9c2009675aae9611bf6a9
JuneJH <15310487388@163.com>
2023-07-15 23:24:30 +0800

    feat(*): 增加惰性求值

--- dataStructure/binaryTree/src/先序遍历.js
+++ dataStructure/binaryTree/src/先序遍历.js
@@ -4,29 +4,42 @@
  * @param {*} result 
  * @returns 
  */
-function preOrder(root,result=[]){
-    if(root === null){
+function preOrder(root, result = []) {
+    if (root === null) {
         return result;
     }
     result.push(root.val);
-    preOrder(root.left,result);
-    preOrder(root.right,result);
+    preOrder(root.left, result);
+    preOrder(root.right, result);
     return result;
 }
-function preOrder4Stack(root){
+function preOrder4Stack(root) {
     const result = [];
-    if(root == null){
+    if (root == null) {
         return result;
     }
     const stack = new Array();
     stack.push(root);
-    while(stack.length > 0){
+    while (stack.length > 0) {
         const currentRoot = stack.pop();
         result.push(currentRoot.val);
         currentRoot.right && stack.push(currentRoot.right);
         currentRoot.left && stack.push(currentRoot.left);
     }
     return result;
+}
+// 利用惰性求值
+function* preOrderGenerator(root) {
+    if (root === null) {
+        return;
+    }
+    yield root.val;
+    yield* preOrderGenerator(root.left);
+    yield* preOrderGenerator(root.right);
+}
+
+function preOrder(root) {
+    return [...preOrderGenerator(root)];
 }
 
 module.exports = {
