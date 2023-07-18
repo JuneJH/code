902474ed96419c51e4a66cef9cf48f44c743b380
JuneJH <15310487388@163.com>
2023-07-18 23:19:32 +0800

    feat(*): 后序遍历栈方式

--- dataStructure/binaryTree/src/__test__/index.test.js
+++ dataStructure/binaryTree/src/__test__/index.test.js
@@ -1,9 +1,12 @@
-const {preOrder,preOrder4Stack,preOrderGeneratorCall} = require("../先序遍历");
-const {    ldr,
+const { preOrder, preOrder4Stack, preOrderGeneratorCall } = require("../先序遍历");
+const { ldr,
     ldr4stack,
-    callLdrGenrotor} = require("../中序遍历");
-const { Node,getTreeDeep} = require("../utils");
-describe("测试二叉树遍历",()=>{
+    callLdrGenrotor } = require("../中序遍历");
+
+const { lrd,
+    lrd4Stack } = require("../后序遍历");
+const { Node, getTreeDeep } = require("../utils");
+describe("测试二叉树遍历", () => {
     const a = new Node("1");
     const b = new Node("2");
     const c = new Node("3");
@@ -13,33 +16,33 @@ describe("测试二叉树遍历",()=>{
     a.left = b;
     a.right = c;
     b.left = d;
-    d.right =e;
-    it("【前序遍历】递归方案",()=>{
-        expect(preOrder(a)).toEqual(["1","2","4","5","3"]);
+    d.right = e;
+    it("【前序遍历】递归方案", () => {
+        expect(preOrder(a)).toEqual(["1", "2", "4", "5", "3"]);
     });
-    it("【前序遍历】栈方案",()=>{
-        expect(preOrderGeneratorCall(a)).toEqual(["1","2","4","5","3"]);
+    it("【前序遍历】栈方案", () => {
+        expect(preOrderGeneratorCall(a)).toEqual(["1", "2", "4", "5", "3"]);
     });
-    it("【前序遍历】迭代器方案",()=>{
-        expect(preOrderGeneratorCall(a)).toEqual(["1","2","4","5","3"]);
-    });
-    
-    it("【中序遍历】递归方案",()=>{
-        expect(ldr(a)).toEqual(["4","5","2","1","3"]);
-    });
-    it("【中序遍历】栈方案",()=>{
-        expect(ldr4stack(a)).toEqual(["4","5","2","1","3"]);
-    });
-    it("【中序遍历】迭代器方案",()=>{
-        expect(callLdrGenrotor(a)).toEqual(["4","5","2","1","3"]);
+    it("【前序遍历】迭代器方案", () => {
+        expect(preOrderGeneratorCall(a)).toEqual(["1", "2", "4", "5", "3"]);
     });
 
-    // it("【后序遍历】递归方案",()=>{
-    //     expect(preOrderGeneratorCall(a)).toEqual(["1","2","4","5","3"]);
-    // });
-    // it("【后序遍历】栈方案",()=>{
-    //     expect(preOrderGeneratorCall(a)).toEqual(["1","2","4","5","3"]);
-    // });
+    it("【中序遍历】递归方案", () => {
+        expect(ldr(a)).toEqual(["4", "5", "2", "1", "3"]);
+    });
+    it("【中序遍历】栈方案", () => {
+        expect(ldr4stack(a)).toEqual(["4", "5", "2", "1", "3"]);
+    });
+    it("【中序遍历】迭代器方案", () => {
+        expect(callLdrGenrotor(a)).toEqual(["4", "5", "2", "1", "3"]);
+    });
+
+    it("【后序遍历】递归方案", () => {
+        expect(lrd(a)).toEqual(["5", "4", "2", "3", "1"]);
+    });
+    it("【后序遍历】栈方案",()=>{
+        expect(lrd4Stack(a)).toEqual(["5", "4", "2", "3", "1"]);
+    });
     // it("【后序遍历】迭代器方案",()=>{
     //     expect(preOrderGeneratorCall(a)).toEqual(["1","2","4","5","3"]);
     // });
--- dataStructure/binaryTree/src/后序遍历.js
+++ dataStructure/binaryTree/src/后序遍历.js
@@ -1,13 +1,42 @@
-function lrd(root,result = []){
-    if(root === null){
-        return ;
+function lrd(root, result = []) {
+    if (root === null) {
+        return;
     }
 
-    lrd(root.left,result);
-    lrd(root.right,result);
+    lrd(root.left, result);
+    lrd(root.right, result);
     result.push(root.val);
     return result;
 }
+
+function lrd4Stack(root) {
+    if (root == null) {
+        return;
+    }
+    const result = [];
+    const nodes = [];
+    let lastNode = null;
+
+    while (root || nodes.length > 0) {
+        while (root) {
+            nodes.push(root);
+            root = root.left;
+        }
+        const node = nodes.pop();
+        if (node.right && node.right !== lastNode) {
+            nodes.push(node);
+            root = node.right;
+        } else {
+            result.push(node.val);
+            lastNode = node;
+        }
+
+    }
+
+    return result;
+}
+
 module.exports = {
-    lrd
+    lrd,
+    lrd4Stack
 }
